19b10a95137b15e9d343d2cad709ee79350ed746
karangorania <karangoraniya@gmail.com>
2022-04-19 18:27:56 +0530

    different networks added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,50 @@
 require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
+
+const { MUMBAI_API_URL, ROPSTEN_API_URL,RINKEBY_API_URL, PRIVATE_KEY, ETHERSCAN_KEY } =
+  process.env;
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.0',
+      },
+      {
+        version: '0.8.0',
+      },
+      {
+        version: '0.8.4',
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: RINKEBY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    ropsten: {
+      url: ROPSTEN_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: ETHEREUM_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    matic: {
+      url: MATIC_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: MUMBAI_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  },
 };
